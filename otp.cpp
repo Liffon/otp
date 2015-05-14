1a1903584a19f505979daea1ab4daaece37e07eb
Liffon <liffon@gmail.com>
2015-05-14 23:55:35 +0200

    Can read message and key from files

--- otp.cpp
+++ otp.cpp
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include <assert.h>
-#include <iostream>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 typedef uint8_t  u8;
 typedef uint16_t u16;
@@ -34,19 +37,69 @@ data* allocateData(size_t size, bool clear = false)
     return result;
 }
 
-data* readKey()
+size_t fileSize(const char* filename)
 {
-    data* result = allocateData(8, true);
+    struct stat buffer = {};
+    int result = stat(filename, &buffer);
 
-    ((u64*)result->bytes)[0] = 0x1234567890abcdef;
-    
+    if(result != -1)
+    {
+        return buffer.st_size;
+    }
+    else
+    {
+        assert(result != -1);
+        return -1;
+    }
+}
+
+data* readFile(const char* filename)
+{
+    size_t filesize = fileSize(filename);
+    data* result = allocateData(filesize, false);
+
+    FILE* handle = fopen(filename, "rb");
+    size_t bytes_read = fread(result->bytes, 1, filesize, handle);
+    assert(bytes_read == filesize);
+
     return result;
+}
+
+data* readFileEnd(const char* filename, size_t size)
+{
+    size_t filesize = fileSize(filename);
+    printf("Filesize is %d bytes (want at least %d bytes).\n", filesize, size);
+
+    if(filesize >= size)
+    {
+        data* result = allocateData(size, true);
+
+        FILE* handle = fopen(filename, "rb");
+        fseek(handle, -size, SEEK_END);
+
+        size_t bytes_read = fread(result->bytes, 1, size, handle);
+        assert(bytes_read == size);
+
+        return result;
+    }
+    else
+    {
+        return 0;
+    }
 }
 
 int main()
 {
-    data* key = readKey();
-    data* message = allocateData(8, true);
+    data* message = readFile("message.txt");
+    data* key = readFileEnd("secret.key", message->length);
+    if(key)
+    {
+        printf("I have both plaintext and a key. Let the encrypting commence!\n");
+    }
+    else
+    {
+        printf("The key is too short. Aborting!\n");
+    }
 
     return 0;
 }
