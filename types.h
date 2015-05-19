70e3558735b04838ca25669f3f27ef13acde2455
Liffon <liffon@gmail.com>
2015-05-19 06:19:07 +0200

    Added reallocateData function to resize data

--- types.h
+++ types.h
@@ -1,5 +1,6 @@
 #ifndef TYPES_H
 
+#include <stdlib.h>
 #include <stdint.h>
 
 typedef uint8_t  u8;
@@ -13,7 +14,7 @@ struct data
     u8 bytes[1]; // NOTE: this is supposed to be longer than 1 byte!
 };
 
-data* allocateData(size_t length, bool clear = false)
+data* allocateData(u64 length, bool clear = false)
 {
     data* result = (data*)malloc(sizeof(u64) + length);
     result->length = length;
@@ -22,6 +23,25 @@ data* allocateData(size_t length, bool clear = false)
     {
         for(u64 i = 0;
             i < length;
+            ++i)
+        {
+            result->bytes[i] = 0;
+        }
+    }
+
+    return result;
+}
+
+data* reallocateData(data* oldData, u64 newLength, bool clear = false)
+{
+    u64 oldLength = oldData->length;
+    data* result = (data*)realloc(oldData, sizeof(u64) + newLength);
+    result->length = newLength;
+
+    if(clear && (newLength > oldLength))
+    {
+        for(u64 i = oldLength;
+            i < newLength;
             ++i)
         {
             result->bytes[i] = 0;
